0e76f9596a3b10f2e0335ff7a0b666f571bd4cc6
BenCinn <pp.rongpan@gmail.com>
2022-07-30 14:17:22 +0700

    initial commit

---
+++ apps/bencinn-website/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'bencinn-website',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/bencinn-website',
+};
